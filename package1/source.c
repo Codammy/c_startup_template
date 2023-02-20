5e33de49bbed65808603990982dcd079b28087f0
Codammy <damisco005@gmail.com>
2023-02-20 15:12:56 -0800

    Committed at Mon 20 Feb 2023 03:12:56 PM PST

--- package1/source.c
+++ package1/source.c
@@ -1,4 +1,4 @@
-#define MAX_VAL 100
+#define MAX_VAL 176
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -7,50 +7,50 @@
 int main(int argc, char *argv[])
 {
         char *buff;
-        size_t fd, len;
+        int fd, len;
 
                 /* checks if corect argument is passed*/
         if (argc != 2)
         {
                 dprintf(2, "Error: Usage >> %s file_name\n", argv[0]);
-                exit(-1);
+                exit(97);
         }
                 /* allocates buffer memory */
         buff = malloc(MAX_VAL * sizeof(char));
         if (buff == NULL)
         {
                 dprintf(2, "Memory not allocated\n");
-                exit(-1);
+                exit(1);
         }
                 /* opening file */
         fd = open("../.tmpfile.txt", O_RDONLY);
-        if (fd == -1)
+        if (fd < 0)
         {
                 dprintf(2, "Can't open file check if tmpfile exist\n");
-                exit(-1);
+                exit(99);
         }
                 /* read from file tmpfile*/
         len = read(fd, buff, MAX_VAL);
         if (len < 0)
         {
                 dprintf(2, "Unable to read file, check modify tmpfile file permissions\n");
-                exit(-1);
+                exit(98);
         }
         close(fd);
 
                 /* create and open template */
-        fd = open(argv[1], O_CREAT | O_RDWR, 0640);
-        if (fd == -1)
+        fd = open(argv[1], O_CREAT | O_RDWR, 0664);
+        if (fd < 0)
         {
                 dprintf(2, "Template not created, check source code\n");
-                exit(-1);
+                exit(99);
         }
                 /* write into template */
         len = write(fd, buff, MAX_VAL);
         if (len < 0)
         {
                 dprintf(2, "Unable to read file, check modify tmpfile file permissions\n");
-                exit(-1);
+                exit(99);
         }
         close(fd);
 
